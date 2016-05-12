f6ad344f13354ad2674c5663f593342197715a76
Michael Grill <Ultramar@michaels-mbp.home>
2016-05-11 21:02:25 -0400

    debugging

--- CarService/src/main/java/com/theironyard/clt/controllers/CarServiceController.java
+++ CarService/src/main/java/com/theironyard/clt/controllers/CarServiceController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -19,9 +21,10 @@ public class CarServiceController {
 
 
     @RequestMapping("/cars")
-    public List<Car> showCars (HttpSession session) {
+    public List<Car> showCars (HttpSession session, HttpServletResponse response)throws IOException {
         List<Car> Cars = cars.getCars();
 
+        response.sendRedirect("/");
         return Cars;
     }
 
